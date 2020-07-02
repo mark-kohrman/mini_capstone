b10934479b57260024720b698df69ffcec27dacc
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-01 21:17:33 -0500

    adding segment and query parameters to file

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,10 @@
 class Api::ProductsController < ApplicationController
 
   def new_product
+    @brand = params["ski_brand"]
+    @price = params["ski_price"].to_i
+    @color = params["ski_color"]
+
     render 'skistore.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
  
   namespace :api do
     get "/skistore" => "products#new_product"
-    
-   end
+    get "/skistore/:ski_brand/:ski_price/:ski_color" => "products#new_product"
+  end
 end
