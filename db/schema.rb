b6ac65edd59315193a7f62828cb48079d0e081a3
Mark-Kohrman <mark.kohrman@gmail.com>
2020-07-07 20:16:27 -0500

    adding data to the seeds file

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_011346) do
+ActiveRecord::Schema.define(version: 2020_07_08_002654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 7, scale: 2
     t.string "image_url"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.create!([
+  {name: "K2", price: "400.0", image_url: nil, description: "royal blue with white stripes"},
+  {name: "K2", price: "400.0", image_url: nil, description: "royal blue with white stripes"},
+  {name: "K2", price: "400.0", image_url: nil, description: "royal blue with white stripes"},
+  {name: "K2", price: "400.0", image_url: nil, description: "royal blue with white stripes"},
+  {name: "K2", price: "400.0", image_url: "hello.com", description: "royal blue with stripes"},
+  {name: "K2", price: "400.0", image_url: "hello.com", description: "royal blue with stripes"},
+  {name: "K2", price: "400.0", image_url: "hello.com", description: "royal blue with stripes"},
+  {name: "Burton", price: "888.0", image_url: "skis.com", description: "orange and red"},
+  {name: "Snowbird", price: "877.0", image_url: "snow.com", description: "real cool skis"}
+])
